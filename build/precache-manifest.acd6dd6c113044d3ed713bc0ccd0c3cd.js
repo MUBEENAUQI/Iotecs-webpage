e756294a510aa7867e25b6f68f09b4b9d0c6e546
mubeenaTechknocks <67516058+mubeenaTechknocks@users.noreply.github.com>
2022-05-28 16:41:55 +0530

    new files

---
+++ build/precache-manifest.acd6dd6c113044d3ed713bc0ccd0c3cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30bd5f13bcf62bb50a82ce95edce1327",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f403a1be21aca13f2755",
+    "url": "/static/css/2.e78a7c17.chunk.css"
+  },
+  {
+    "revision": "6319f2142008b9e21941",
+    "url": "/static/css/main.ece445d9.chunk.css"
+  },
+  {
+    "revision": "f403a1be21aca13f2755",
+    "url": "/static/js/2.350926e8.chunk.js"
+  },
+  {
+    "revision": "934e00ac9de52f516d20d0d76cc1921d",
+    "url": "/static/js/2.350926e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6319f2142008b9e21941",
+    "url": "/static/js/main.dc0da397.chunk.js"
+  },
+  {
+    "revision": "fbf771ac11559020264f",
+    "url": "/static/js/runtime-main.352374c8.js"
+  }
+]);
